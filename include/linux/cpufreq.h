fbd15128bf0e8aac4de82b4937b4d68c22e433bf
Anas <anaskarbila@gmail.com>
2015-04-11 06:00:40 -0700

    add pegasusq governor and enable it in defconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -381,6 +381,10 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
+extern struct cpufreq_governor cpufreq_gov_pegasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_pegasusq)
+
 #endif
 
 
